536ec8c9b9e9a5d305fb0fd1d57e799a3a17f00d
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-07 20:00:03 +0300

    get/delete user by id

---
+++ ations/auth/DeleteUserByIdAction.js
@@ -0,0 +1,38 @@
+import IAction from '../IAction.js';
+
+import UID from '../../lib/UID.js';
+import AuthService from '../../services/AuthService.js';
+import UserRepository from '../../repositories/UserRepository.js';
+import AppError, { ERROR_PRESETS } from '../../errors/AppError.js';
+
+class DeleteUserByIdAction extends IAction {
+  constructor() {
+    super();
+
+    this.authService = new AuthService(new UserRepository());
+  }
+
+  get accessTag() {
+    return 'auth:deleteuserbyid';
+  }
+
+  run = async (req, res) => {
+    const { id } = this.validate(req.params);
+
+    await this.authService.deleteUserById(id);
+
+    return res.json({ success: true, message: `Seccesful deleted user by id: ${id}` });
+  };
+
+  validate(input) {
+    const { id } = input;
+
+    if (!UID.isValid(id)) {
+      throw new AppError(ERROR_PRESETS.INVALID_INPUT('Id', id, 'is invalid'));
+    }
+
+    return { id };
+  }
+}
+
+export default DeleteUserByIdAction;
--- routers/auth.route.js
+++ routers/auth.route.js
@@ -2,17 +2,21 @@ import { Router } from 'express';
 import RegistrationAction from '../ations/auth/RegistrationAction.js';
 import LoginAction from '../ations/auth/LoginAction.js';
 import GetUsersAction from '../ations/auth/GetUsersAction.js';
+import GetUserByIdAction from '../ations/auth/GetUserByIdAction.js';
+import DeleteUserByIdAction from '../ations/auth/DeleteUserByIdAction.js';
 
 const authRouter = new Router();
 
 const registrationAction = new RegistrationAction();
 const loginAction = new LoginAction();
 const getUsersAction = new GetUsersAction();
+const getUserByIdAction = new GetUserByIdAction();
+const deleteUserByIdAction = new DeleteUserByIdAction();
 
 authRouter.post('/registration', registrationAction.run);
 authRouter.post('/login', loginAction.run);
 authRouter.get('/users', getUsersAction.run);
-// authRouter.get('/users/:id', authController.getUserById);
-// authRouter.delete('/users/:id', authController.deleteUserById);
+authRouter.get('/users/:id', getUserByIdAction.run);
+authRouter.delete('/users/:id', deleteUserByIdAction.run);
 
 export default authRouter;
